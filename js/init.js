00d5a9eba1a9b35302ff3fe904a211b7ba02dcfb
Brian Dorton <dorton@gmail.com>
2016-10-13 05:02:28 -0500

    mockup for law site

--- js/init.js
+++ js/init.js
@@ -3,6 +3,24 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({
+      full_width: true
+    });
+    autoplay()
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    }
+  );
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
